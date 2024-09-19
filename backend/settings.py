7c27924e8cb4055e680937950eb1deca38d92e7b
Brajenko <goshabrajenko@gmail.com>
2024-09-19 08:52:53 +0300

    devops

--- backend/settings.py
+++ backend/settings.py
@@ -134,6 +134,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', )
 
 
 # Media
---
+++ setup.sh
@@ -0,0 +1,5 @@
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser --noinput
+python manage.py collectstatic --noinput
