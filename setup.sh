fa794f473050d22346213e847a09c1d41a9f7704
Brajenko <goshabrajenko@gmail.com>
2024-09-19 11:44:55 +0300

    q

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py flush
+python manage.py flush --noinput
 python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
